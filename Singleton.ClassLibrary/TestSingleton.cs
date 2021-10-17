6bee484a52998ea1e97a21a883334a1ac14cd84e
Valentin Virot <valentin.virot@diiage.org>
2021-10-17 12:35:54 +0200

    Added Singleton design pattern and it's unit test

---
+++ Singleton.ClassLibrary/TestSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton.ClassLibrary
+{
+    public class TestSingleton
+    {
+        private TestSingleton() { }
+
+        private static TestSingleton _instance;
+
+        private static readonly object _lock = new object();
+
+        public static TestSingleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                lock(_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new TestSingleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
+
---
+++ Singleton.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton.ClassLibrary;
+
+namespace Singleton.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestSingleton s1 = TestSingleton.GetInstance();   
+            TestSingleton s2 = TestSingleton.GetInstance();
+
+            Assert.AreEqual(s2, s1);
+        }
+    }
+}
+
